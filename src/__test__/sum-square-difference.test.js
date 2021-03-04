e009c081a24e0850bb9abe4761fd36ca4ef8fd29
Esteban Ladino <47506498+Esteban-Ladino@users.noreply.github.com>
2021-03-04 17:24:59 -0500

    Largest product problem solved

--- src/__test__/sum-square-difference.test.js
+++ src/__test__/sum-square-difference.test.js
@@ -1,6 +1,12 @@
 import { sumSquareDifference } from '../sum-square-difference'
 
 it(`should find the difference between the sum of the squares
+ of the first 10 numbers and the square of the sum.`,
+() => {
+  expect(sumSquareDifference(10)).toBe(2640)
+})
+
+it(`should find the difference between the sum of the squares
  of the first 100 numbers and the square of the sum.`,
 () => {
   expect(sumSquareDifference(100)).toBe(25164150)
---
+++ src/largest-product.js
@@ -0,0 +1,17 @@
+export const largestProduct = (num, limit) => {
+  let adjacentDigits = 0;
+  let digitsProduct = 0
+  let greatestproduct = 0;
+
+  for (let i = 0; i <= num.length - limit; i++) {
+    adjacentDigits = num.substring(i, i+limit);
+
+    digitsProduct = [...adjacentDigits]
+      .reduce((acumulator, value) => acumulator * value);
+
+    if (digitsProduct > greatestproduct) {
+      greatestproduct = digitsProduct;
+    }
+  }
+  return greatestproduct;
+}
